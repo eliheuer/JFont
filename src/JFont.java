8863be12e1b43d9ca1f3c06ad363c3a7bac23c37
Eli Heuer <eliheuer@gmail.com>
2017-11-08 01:01:57 -0500

    Adding basic bezier method

--- src/JFont.java
+++ src/JFont.java
@@ -3,6 +3,26 @@ public class JFont {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World! ");
+        print("Welcome to JFont Alpha!");
+        int n = 512;
+        int t = 256;
+        int bez = bezier(n,t);
+        print("bez = "+bez);
+    }
+
+    /** Basic bezier method */
+    public static int bezier(int n, int t) {
+    int sum = 0;
+    int k = 12;
+    for(int i=0; i<n; i++) {
+        sum +=
+        (k*(n-k))*(1-t);
+        print(" "+sum);
+    }
+    return sum;
+    }
+
+    public static void print(String m) {
+        System.out.println(m);
     }
 }
