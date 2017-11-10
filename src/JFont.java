df5152b756d1edd5fe7729a8837280dff392925b
Eli Heuer <eliheuer@gmail.com>
2017-11-10 10:37:26 -0500

    adding cli menu

--- src/JFont.java
+++ src/JFont.java
@@ -1,32 +1,75 @@
-/** JFont, a simple Java UFO font editor. */
+/* JFont, a simple Java UFO font editor. */
 
 import java.awt.Point;                  // For (x,y) points
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileInputStream;         // TODO Info here
+import java.io.IOException;             // TODO Info here
 
-import java.util.Arrays;
+import java.util.Arrays;                // TODO Info here
 import java.util.Scanner;               // Get input
 import java.util.Random;                // Random
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.JAXBContext;      // TODO Info here
+import javax.xml.bind.JAXBElement;      // TODO Info here
+import javax.xml.bind.JAXBException;    // TODO Info here
+import javax.xml.bind.Marshaller;       // TODO Info here
+import javax.xml.bind.Unmarshaller;     // TOdo Info here
 
 public class JFont {
 
     public static void main(String[] args) {
 
-	// Start input
-	Scanner in = new Scanner(System.in);
+	// Setup
+        int mainMenuChoice;
+        Scanner input = new Scanner(System.in);
 
-	// cli();
-	// Debug tools
+	// Welcome
+	prints("\nWelcome to JFont, a simple UFO editor.\n");
+
+	// Main menu
+        mainMenuChoice = mainMenu(input);
+	System.out.println("mainMenuChoice: "+mainMenuChoice);
+
+	// Task router
+	taskRouter(mainMenuChoice);
+
+	// Debug menu
 	debug();
 	
-    } //END Main
+    } //END Main Method
+
+    /** Main Menu */
+    public static int mainMenu(Scanner input) {
+	
+	// Set menu variables
+	int selection;
+
+	// Display currently selected UFO file.
+	System.out.println("(Currently Editing: Sample.UFO)\n");
+
+	// Display Menu 
+        System.out.println("Choose from these choices");
+        System.out.println("-------------------------");
+        System.out.println("1 - Load new UFO");
+        System.out.println("2 - Save current UFO");
+        System.out.println("3 - Export font  current UFO");
+        System.out.println("4 - Edit glyphs");
+        System.out.println("5 - Edit UFO metadata");
+        System.out.println("6 - Encrypt a number");
+        System.out.println("7 - Decrypt a number");
+        System.out.println("8 - Help");
+	System.out.println("9 - Quit\n");
+
+	// Get input
+	System.out.print("Enter a number: ");
+        selection = input.nextInt();
+        return selection;    
+    }
+
+    /** Task router */
+    public static void taskRouter(int task) {
+	System.out.println("In task router, task is: " + task);
+    }
 
     /** Print Point */
     public static void printPoint(Point p) {
@@ -66,7 +109,7 @@ public class JFont {
 
     /** Debug */
     public static void debug() {	
-        prints("Welcome to JFont Alpha!");
+        prints("Welcome to The debug menu.");
         int n = 512;
         int t = 256;
         int bez = bezier(n,t);
